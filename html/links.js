1e412eb5407fc428ef89c480ea1710ef41728f99
Liam-P <83415707+Liam-Perreault@users.noreply.github.com>
2021-05-02 14:49:02 -0400

    Add files via upload

---
+++ html/links.js
@@ -0,0 +1,10 @@
+
+
+document.write(' <div class="uni_dropdown">\n\
+                    <button class="uni_dropbtn">PROJETS</button>\n\
+                    <div class="uni_dropdown-content">\n\
+                        <a href="../html/Poem.html">Poème</a>\n\
+                        <a href="../html/Liste.html">Liste</a>\n\
+                        <a href="../html/voyage.html">Voyage</a>\n\
+                     </div>\n\
+                </div> ');
