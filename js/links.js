7e8fb19dd8411af0392aef62ccc4a647874496dc
Liam-P <83415707+Liam-Perreault@users.noreply.github.com>
2021-05-05 21:29:31 -0400

    Create links.js

---
+++ js/links.js
@@ -0,0 +1,18 @@
+
+
+document.write(' <div class="uni_code_bg"> </div> \n\
+                <div class="uni_bar">\n\
+                    <a href="../index.html" class="uni_name">\n\
+                        <img class="uni_logo" src="../img/liam.svg"/> Liam Perreault </a>\n\
+                    <a class="uni_button" href="../html/Bibliographie.html">\n\
+                        <button class="uni_bibliographie">BIBLIOGRAPHIE</button>\n\
+                    </a> \n\
+                </div>\n\
+                <div class="uni_dropdown">\n\
+                    <button class="uni_dropbtn">PROJETS</button>\n\
+                    <div class="uni_dropdown-content">\n\
+                        <a href="../html/Poem.html">Poème</a>\n\
+                        <a href="../html/Liste.html">Liste</a>\n\
+                        <a href="../html/Voyage.html">Voyage</a>\n\
+                     </div>\n\
+                </div> ');
