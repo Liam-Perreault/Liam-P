abd13a10ea235ec3875c216d0bee7bf82e4400a3
Liam-P <83415707+Liam-Perreault@users.noreply.github.com>
2021-05-28 22:06:16 -0400

    Add files via upload

---
+++ js/links.js
@@ -0,0 +1,33 @@
+
+
+document.write(' <div class="uni_code_bg"> </div> \n\
+                <div class="uni_bar">\n\
+                    <a href="../index.html" class="uni_name">\n\
+                        <img class="uni_logo" src="../img/liam.svg"/> Liam Perreault </a>\n\
+                    </div>\n\
+                    <div class="uni_dropdown">\n\
+                        <button class="uni_dropbtn">PROJETS</button>\n\
+                        <div class="uni_dropdown-content">\n\
+                            <a href="../html/Poem.html">Poème</a>\n\
+                            <a href="../html/Liste.html">Liste</a>\n\
+                            <a href="../html/Voyage.html">Voyage</a>\n\
+                        </div>\n\
+                    </div>\n\
+                    <div class="uni_dropdown_2">\n\
+                        <button class="uni_dropbtn_2">TRAVAIL</button>\n\
+                        <div class="uni_dropdown-content_2">\n\
+                            <a href="../html/Travail_1-1.html">Travail 1 - 1</a>\n\
+                            <a href="../html/Travail_1-2.html">Travail 1 - 2</a>\n\
+                            <a href="../html/Travail_1-3.html">Travail 1 - 3</a>\n\
+                            <a href="../html/Travail_2.html">Travail 2 </a>\n\
+                        </div>\n\
+                    </div>\n\
+                    <div class="uni_dropdown_3">\n\
+                        <button class="uni_dropbtn_3">2.6</button>\n\
+                        <div class="uni_dropdown-content_3">\n\
+                            <a href="../html/Quiz.html">Quiz</a>\n\
+                            <a href="../html/Mad_libs.html">Mad Libs</a>\n\
+                        </div>\n\
+                    </div>\n\
+                </div>\n\
+                </div> ');
